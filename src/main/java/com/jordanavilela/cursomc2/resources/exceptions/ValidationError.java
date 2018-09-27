5ee0246a5c732c178a9c6449ab80423a356b40e9
jordanavi <jordanavilela01@gmail.com>
2018-09-27 20:49:15 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/jordanavilela/cursomc2/resources/exceptions/ValidationError.java
+++ src/main/java/com/jordanavilela/cursomc2/resources/exceptions/ValidationError.java
@@ -1,5 +1,24 @@
 package com.jordanavilela.cursomc2.resources.exceptions;
 
-public class ValidationError extends StandardError {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String messagem) {
+		errors.add(new FieldMessage(fieldName, messagem));
+	}
+	
 }
