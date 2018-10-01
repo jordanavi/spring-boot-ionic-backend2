d965fd400e7afa7efee15e11515036da9bd2f60f
jordanavi <jordanavilela01@gmail.com>
2018-10-01 13:45:20 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/jordanavilela/cursomc2/domain/Cliente.java
+++ src/main/java/com/jordanavilela/cursomc2/domain/Cliente.java
@@ -40,8 +40,7 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	public Cliente() {
-		
+	public Cliente() {	
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -50,7 +49,7 @@ public class Cliente implements Serializable {
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null : tipo.getCod();
 	}
 
 	public Integer getId() {
