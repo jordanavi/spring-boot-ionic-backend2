b87eb12abf0f0e2242693e169e46128f91ca8cf1
jordanavi <jordanavilela01@gmail.com>
2018-10-04 23:20:26 -0300

    Validadacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/jordanavilela/cursomc2/dto/ClienteDTO.java
+++ src/main/java/com/jordanavilela/cursomc2/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.jordanavilela.cursomc2.domain.Cliente;
+import com.jordanavilela.cursomc2.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
