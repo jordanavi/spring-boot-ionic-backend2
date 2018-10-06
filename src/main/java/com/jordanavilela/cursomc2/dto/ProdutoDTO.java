43e561c083bc1330046d62fc1211dca10066576b
jordanavi <jordanavilela01@gmail.com>
2018-10-06 12:03:57 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/jordanavilela/cursomc2/dto/ProdutoDTO.java
@@ -0,0 +1,46 @@
+package com.jordanavilela.cursomc2.dto;
+
+import java.io.Serializable;
+
+import com.jordanavilela.cursomc2.domain.Produto;
+
+public class ProdutoDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private double preco;
+	
+	public ProdutoDTO() {
+	}
+	
+	public ProdutoDTO(Produto obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		preco = obj.getPreco();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+}
--- src/main/java/com/jordanavilela/cursomc2/repositories/ClienteRepository.java
+++ src/main/java/com/jordanavilela/cursomc2/repositories/ClienteRepository.java
@@ -1,16 +1,15 @@
 package com.jordanavilela.cursomc2.repositories;
 
-import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jordanavilela.cursomc2.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
-	//@Transactional(readOnly=true)
-	@Transactional
+	@Transactional(readOnly=true)
 	Cliente findByEmail(String email);
 }
